03141e0191e4cd4990d2791fef5f2145d594123f
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-11-12 21:04:46 +0000

    - Upgraded to use Date::Persian::Simple v0.14.

--- lib/Calendar/Persian.pm
+++ lib/Calendar/Persian.pm
@@ -1,6 +1,6 @@
 package Calendar::Persian;
 
-$Calendar::Persian::VERSION   = '0.29';
+$Calendar::Persian::VERSION   = '0.30';
 $Calendar::Persian::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Calendar::Persian - Interface to Persian Calendar.
 
 =head1 VERSION
 
-Version 0.29
+Version 0.30
 
 =cut
 
